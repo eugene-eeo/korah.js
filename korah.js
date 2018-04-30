f4ab9c3262f6ed52dbb30df240bb7784393b22d9
Eeo Jun <141bytes@gmail.com>
2018-04-30 11:56:01 +0100

    update

--- korah.js
+++ korah.js
@@ -1,41 +1,44 @@
 !function() {
-  'use strict';
+    'use strict';
 
-  function normalise(nodes) {
-    return nodes.reduce(function(rv, e) {
-      return rv.concat(Array.isArray(e)
-        ? normalise(e)
-        : e.nodeType
-          ? [e]
-          : [document.createTextNode(e)]);
-    }, []);
-  }
+    var type = {}.toString,
+        ARRAY  = '[object Array]',
+        OBJECT = '[object Object]';
 
-  function createNode(tag, attrs, children) {
-    var node = document.createElement(tag);
-    normalise(children).forEach(node.appendChild.bind(node));
-    for (var i in attrs)
-      node.setAttribute(i, attrs[i]);
-    return node;
-  }
-
-  var type = {}.toString;
-
-  window.kr = function(tag, attrs, children) {
-    if (attrs && type.call(attrs) !== '[object Object]') {
-      children = attrs;
-      attrs = {};
+    function normalise(nodes) {
+        nodes = (type.call(nodes) === ARRAY) ? nodes : [nodes];
+        return nodes.reduce(function(rv, e) {
+            return rv.concat((type.call(e) === ARRAY)
+                ? normalise(e)
+                : (e.nodeType
+                    ? [e]
+                    : [document.createTextNode(e)]));
+        }, []);
     }
-    return createNode(tag, attrs, children
-      ? Array.isArray(children)
-        ? children
-        : [children]
-      : []);
-  };
 
-  kr.addTag = function(tag) {
-    kr[tag] = function(attrs, children) {
-      return kr(tag, attrs, children);
+    function createNode(tag, attrs, children) {
+        var node = document.createElement(tag);
+        children.forEach(node.appendChild.bind(node));
+        for (var i in attrs)
+            node.setAttribute(i, attrs[i]);
+        return node;
+    }
+
+    window.kr = function(tag, attrs, children) {
+        if (attrs && type.call(attrs) !== OBJECT) {
+            children = attrs;
+            attrs = {};
+        }
+        return createNode(
+            tag,
+            attrs,
+            normalise(children || [])
+        );
     };
-  };
+
+    kr.addTag = function(tag) {
+        kr[tag] = function(attrs, children) {
+            return kr(tag, attrs, children);
+        };
+    };
 }();
